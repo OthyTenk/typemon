178b7af914d8371169779a58099d5f06d4471310
Othy Tenk <othy.tenk@gmail.com>
2026-08-24 14:16:38 +0800

    chore(typemon): fix deprecated URL parse

--- apps/web/server.ts
+++ apps/web/server.ts
@@ -1,5 +1,4 @@
 import { createServer } from "http"
-import { parse } from "url"
 import next from "next"
 import { Server, Socket } from "socket.io"
 
@@ -13,10 +12,30 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const httpServer = createServer(async (req, res) => {
     try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url!, true)
-      await handle(req, res, parsedUrl)
+      const url = new URL(req.url!, `http://${req.headers.host || "localhost"}`)
+      const query: Record<string, string | string[]> = {}
+      url.searchParams.forEach((value, key) => {
+        const existing = query[key]
+        if (existing !== undefined) {
+          if (Array.isArray(existing)) {
+            existing.push(value)
+          } else {
+            query[key] = [existing, value]
+          }
+        } else {
+          query[key] = value
+        }
+      })
+
+      const parsedUrl = {
+        pathname: url.pathname,
+        query,
+        search: url.search,
+        hash: url.hash,
+        href: url.href,
+      }
+
+      await handle(req, res, parsedUrl as any)
     } catch (err) {
       console.error("Error occurred handling", req.url, err)
       res.statusCode = 500
@@ -24,42 +43,44 @@ app.prepare().then(() => {
     }
   })
 
-  const io = new Server(httpServer)
+  if (dev) {
+    const io = new Server(httpServer)
 
-  io.on("connection", (socket: Socket) => {
-    console.log("Client connected", socket.id)
+    io.on("connection", (socket: Socket) => {
+      console.log("Client connected", socket.id)
 
-    socket.on("join-game", (gameCode: string) => {
-      socket.join(gameCode)
-      console.log(`Socket ${socket.id} joined game ${gameCode}`)
+      socket.on("join-game", (gameCode: string) => {
+        socket.join(gameCode)
+        console.log(`Socket ${socket.id} joined game ${gameCode}`)
+      })
+
+      socket.on("leave-game", (gameCode: string) => {
+        socket.leave(gameCode)
+        console.log(`Socket ${socket.id} left game ${gameCode}`)
+      })
+
+      socket.on("typing", (data: any) => {
+        // Broadcast to everyone in the room except the sender
+        socket.to(data.gameCode).emit("opponent-position", data)
+      })
+
+      socket.on("game-start", (data: any) => {
+        io.to(data.gameCode).emit("game-starts-in", data)
+      })
+
+      socket.on("player-joined", (data: any) => {
+        io.to(data.gameCode).emit("has-joined-game", data)
+      })
+
+      socket.on("game-finish", (data: any) => {
+        socket.to(data.gameCode).emit("game-finish", data)
+      })
+
+      socket.on("disconnect", () => {
+        console.log("Client disconnected", socket.id)
+      })
     })
-
-    socket.on("leave-game", (gameCode: string) => {
-      socket.leave(gameCode)
-      console.log(`Socket ${socket.id} left game ${gameCode}`)
-    })
-
-    socket.on("typing", (data: any) => {
-      // Broadcast to everyone in the room except the sender
-      socket.to(data.gameCode).emit("opponent-position", data)
-    })
-
-    socket.on("game-start", (data: any) => {
-      io.to(data.gameCode).emit("game-starts-in", data)
-    })
-
-    socket.on("player-joined", (data: any) => {
-      io.to(data.gameCode).emit("has-joined-game", data)
-    })
-
-    socket.on("game-finish", (data: any) => {
-      socket.to(data.gameCode).emit("game-finish", data)
-    })
-
-    socket.on("disconnect", () => {
-      console.log("Client disconnected", socket.id)
-    })
-  })
+  }
 
   httpServer
     .once("error", (err) => {
